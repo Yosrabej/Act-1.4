06a0ef253fdbfceedefdbe178c9ffd93d1d11d40
Yosrabej <79087257+Yosrabej@users.noreply.github.com>
2021-10-10 15:56:11 +0100

    Update Poster-un-article.php

--- Poster-un-article.php
+++ Poster-un-article.php
@@ -1,11 +1,11 @@
-<?php include ('header.php');?>
+<?php include('header.php'); ?>
 <form method="post" action="articles.php">
-<input type="text" name='titre' value="ajouter le titre"/>
-<input type="text" name='texte' value="ajouter le texte"/>
-<input type="text" name='auteur' value="auteur"/>
-<input type="date" name='date_pub' value="date"/>
-<input type="submit" value="Publier" />
+    <input type="text" name='titre' placeholder="ajouter le titre" />
+    <input type="text" name='texte' placeholder="ajouter le texte" />
+    <input type="text" name='auteur' placeholder="auteur" />
+    <input type="date" name='date_pub' placeholder="date" />
+    <input type="submit" value="Publier" />
 </form>
 
 
-<?php include ('footer.php');?>
+<?php include('footer.php'); ?>
