a70baefafaa9ebfffc5683042b991c1603d6f404
Yosrabej <79087257+Yosrabej@users.noreply.github.com>
2021-10-11 09:33:32 +0100

    Update articles.sql

--- articles.sql
+++ articles.sql
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS `articles` (
   `titre` varchar(255) NOT NULL,
   `texte` text NOT NULL,
   `auteur` varchar(255) NOT NULL,
-  `date de publication` date NOT NULL,
+  `date_pub` date NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
